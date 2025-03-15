7e3eab25f2e6e4fc53359a67a25d207afc4abfef
Boris Nyilindekwe <borisnyilindekwe@gmail.com>
2025-03-15 02:17:27 +0200

    fix: disable eslint rule for unused vars in search component

--- frontend/src/components/search.tsx
+++ frontend/src/components/search.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Search, Loader2, X } from 'lucide-react';
 import React, { useState, useEffect } from 'react';
 import { Business } from '@/Home';
